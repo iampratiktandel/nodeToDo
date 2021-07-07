15d5501ab91c32d8a98ccac01f6c8a65d73983be
iampratiktandel <iampratiktandel@gmail.com>
2021-07-07 23:45:28 +0530

    add: to-do controller

---
+++ controllers/todoController.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+  
+  app.get('/todo', (req, res) => {
+
+  });
+
+  app.post('/todo', (req, res) => {
+
+  });
+
+  app.delete('/todo', (req, res) => {
+
+  });
+}
