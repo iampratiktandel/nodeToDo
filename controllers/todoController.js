44adf0a6d18da38c32a721b898f6f1f20c569d63
iampratiktandel <iampratiktandel@gmail.com>
2021-07-10 20:46:09 +0530

    feat: add get, post api update: assets

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,14 +1,23 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var data = [{item: 'eat'}, {item: 'sleep'}, {item: 'code'}];
+
 module.exports = function(app) {
   
   app.get('/todo', (req, res) => {
-
+    res.render('todo', {todos: data});
   });
 
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodedParser, (req, res) => {
+    data.push(req.body);
+    res.json(data);
   });
 
-  app.delete('/todo', (req, res) => {
-
+  app.delete('/todo/:item', (req, res) => {
+    data = data.filter(function(todo) {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
   });
 }
